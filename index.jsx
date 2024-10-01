0a6797f5f288487548a995db1708ae5b8b73602c
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-10-01 16:04:52 +0200

    managed to say hello

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDom from "react-dom/client"
+
+const test = (
+    
+        <h1>hello</h1>
+        
+)
+
+ReactDom.createRoot(document.getElementById("root")).render(test);
